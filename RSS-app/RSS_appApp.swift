b5ea1001c41ae242b4173c0fca7d34eb4f6d4363
Naonaonari <naonao12179@gmail.com>
2025-12-14 17:13:36 +0900

    Initial Commit

---
+++ RSS-app/RSS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_appApp.swift
+//  RSS-app
+//
+//  Created by 谷本直柔 on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct RSS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
